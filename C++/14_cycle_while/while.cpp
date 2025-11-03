4dd939825c32acce250ba652edff358f95b1ac7b
Ivan <ivan.gorokhovsky@gmail.com>
2025-11-03 20:34:30 +0300

    Update while.cpp

--- C++/14_cycle_while/while.cpp
+++ C++/14_cycle_while/while.cpp
@@ -4,6 +4,28 @@
 using namespace std;
 
 int main() {
-SetConsoleOutputCP(65001);
+    SetConsoleOutputCP(65001);
+    // int counter = 20;
+    // while (number > 10) {
+    //     cout << number << endl;
+    //     number--;
+    // }
 
+    // int counter = 0;
+    // while (number < 10) {
+    //     cout << number << endl;
+    //     number++;
+    // }
+
+    // int counter = 0;
+    // while (counter <= 10) {
+    //     cout << counter << endl;
+    //     counter++;
+    // }
+
+    // int counter = 20;
+    // while (counter >= 10) {
+    //     cout << counter << endl;
+    //     counter--;
+    // }
 }
